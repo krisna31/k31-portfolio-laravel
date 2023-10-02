2b92fa48d9cb1114f33dc6fac3ac38c192c4ed8e
Krisna31 <krisnaaaputraaa@gmail.com>
2023-10-02 20:33:01 +0700

    FE done

--- resources/js/Pages/Portfolio/Overlay.tsx
+++ resources/js/Pages/Portfolio/Overlay.tsx
@@ -26,9 +26,11 @@ type props = {
     setIsControlling: React.Dispatch<React.SetStateAction<boolean>>,
     isZooming: boolean,
     setIsZooming: React.Dispatch<React.SetStateAction<boolean>>
+    isShowStats: boolean,
+    setIsShowStats: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const Overlay = ({ isControlling, setIsControlling, isZooming, setIsZooming }: props) => {
+export const Overlay = ({ isControlling, setIsControlling, isZooming, setIsZooming, isShowStats, setIsShowStats }: props) => {
     const scroll = useScroll();
     const [opacityFirstSection, setOpacityFirstSection] = useState(1);
     const [opacitySecondSection, setOpacitySecondSection] = useState(1);
@@ -51,6 +53,11 @@ export const Overlay = ({ isControlling, setIsControlling, isZooming, setIsZoomi
             <div className="w-screen">
 
                 <nav className="fixed top-0 right-0 backdrop-blur-sm rounded m-5 gap-3 flex flex-col">
+                    <label className="relative inline-flex items-center cursor-pointer">
+                        <input type="checkbox" className="sr-only peer" onChange={_ => setIsShowStats(prev => !prev)} checked={isShowStats} />
+                        <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
+                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Stats</span>
+                    </label>
                     <label className="relative inline-flex items-center cursor-pointer">
                         <input type="checkbox" className="sr-only peer" onChange={_ => setIsControlling((prev: any) => {
                             prev && setIsZooming(false);
@@ -137,7 +144,7 @@ export const Overlay = ({ isControlling, setIsControlling, isZooming, setIsZoomi
                                 </svg>
                             </a>
 
-                            <a href="https://wa.me/6289525336399" title="Link Whatsapp Krisna" className="text-gray-500 hover:text-gray-900 dark:hover:text-white" target="_blank" rel="noopener noreferrer">
+                            <a href="https://wa.me/628982509595" title="Link Whatsapp Krisna" className="text-gray-500 hover:text-gray-900 dark:hover:text-white" target="_blank" rel="noopener noreferrer">
                                 <svg className="w-5 h-4 md:w-7 md:h-7 lg:w-9 lg:h-9 text-green-400 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                                     <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path>
                                 </svg>
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,30 +1,41 @@
 import { Link, Head } from '@inertiajs/react';
-import { Box, Environment, OrbitControls, ScrollControls, Sky } from "@react-three/drei";
+import { Box, Environment, OrbitControls, ScrollControls, Sky, Sparkles, SpotLight, Stars, StatsGl } from "@react-three/drei";
 import { PageProps } from '@/types';
 import { Canvas } from '@react-three/fiber';
 import { Overlay } from './Portfolio/Overlay';
 import { BG } from '@/Components/BG';
 import { Suspense, useEffect, useState } from 'react';
 import Loader from '@/Components/Loader';
+import MyBG from '@/Components/MyBG';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
     const [isControlling, setIsControlling] = useState<boolean>(true);
+    const [isShowStats, setIsShowStats] = useState<boolean>(false);
     const [isZooming, setIsZooming] = useState<boolean>(false);
 
     return (
         <>
             <Head title="Welcome" />
-            <Canvas shadows camera={{ position: [0, 0, 15] }}>
+            <Canvas shadows camera={{ position: [0, 0, 8], zoom: 2 }} >
+                {isShowStats && <StatsGl />}
                 <Suspense fallback={<Loader />}>
-                    <ambientLight intensity={0.8} />
+                    <ambientLight intensity={1} />
+                    <Stars radius={30} depth={50} count={1000} factor={4} saturation={1} fade speed={.3} />
+                    {/* <Sparkles
+                        color="orange"
+                        count={1000}
+                        noise={1}
+                        opacity={.3}
+                        speed={0.3}
+                    /> */}
                     {isControlling && (
                         <OrbitControls enableZoom={isZooming} autoRotate />
                     )}
                     <ScrollControls pages={5} damping={0.12}>
-                        <Overlay isControlling={isControlling} setIsControlling={setIsControlling} isZooming={isZooming} setIsZooming={setIsZooming} />
-                        <BG />
-                        {/* <Sky sunPosition={[100, 20, 100]} distance={99999999} />
-                        <Environment preset='dawn' /> */}
+                        <Overlay isControlling={isControlling} setIsControlling={setIsControlling} isZooming={isZooming} setIsZooming={setIsZooming} isShowStats={isShowStats} setIsShowStats={setIsShowStats} />
+                        <MyBG />
+                        <Sky sunPosition={[100, 20, 100]} distance={99999999} />
+                        {/* <Environment preset='dawn' /> */}
                     </ScrollControls>
                 </Suspense>
             </Canvas>
