09f3143e2069372224dfe20e927e2caf0366de3b
Krisna31 <krisnaaaputraaa@gmail.com>
2023-10-04 15:14:06 +0700

    FE DOne with all bug reference from https://krisna31.github.io/k31-portfolio-rtsthr/

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -3,19 +3,48 @@ import { Box, Environment, OrbitControls, ScrollControls, Sky, Sparkles, SpotLig
 import { PageProps } from '@/types';
 import { Canvas } from '@react-three/fiber';
 import { Overlay } from './Portfolio/Overlay';
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import Loader from '@/Components/Loader';
-import MyBG from '@/Components/MyBG';
+import { Bg } from '@/Components/Bg';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
     const [isControlling, setIsControlling] = useState<boolean>(true);
     const [isShowStats, setIsShowStats] = useState<boolean>(false);
     const [isZooming, setIsZooming] = useState<boolean>(false);
 
+    useEffect(() => {
+        const divTouch = document.querySelector('canvas')?.nextElementSibling as HTMLElement;
+        !isControlling && divTouch && divTouch.style.removeProperty('touch-action');
+    }, [isControlling])
+
     return (
         <>
             <Head title="Welcome">
+                <link rel="icon" type="image/svg+xml+ico" href="/assets/pictures/logo.ico" />
             </Head>
+            <nav className="absolute top-0 right-0 backdrop-blur-sm rounded m-5 gap-3 flex flex-col z-50">
+                <label className="relative inline-flex items-center cursor-pointer">
+                    <input type="checkbox" className="sr-only peer" onChange={_ => setIsShowStats(prev => !prev)} checked={isShowStats} />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
+                    <span className="ml-3 text-sm font-medium text-white">Stats</span>
+                </label>
+                <label className="relative inline-flex items-center cursor-pointer">
+                    <input type="checkbox" className="sr-only peer" onChange={_ => setIsControlling((prev: any) => {
+                        prev && setIsZooming(false);
+                        return !prev;
+                    })} checked={isControlling} />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-teal-300 dark:peer-focus:ring-teal-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
+                    <span className="ml-3 text-sm font-medium text-white">Control</span>
+                </label>
+                <label className="relative inline-flex items-center cursor-pointer">
+                    <input type="checkbox" className="sr-only peer" onChange={_ => setIsZooming((prev: any) => {
+                        !prev && setIsControlling(true);
+                        return !prev;
+                    })} checked={isZooming} />
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                    <span className="ml-3 text-sm font-medium text-white">Zoom</span>
+                </label>
+            </nav>
             <Canvas shadows camera={{ position: [0, 0, 8], zoom: 2 }} >
                 {isShowStats && <StatsGl />}
                 <Suspense fallback={<Loader />}>
@@ -26,12 +55,13 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                     )}
                     <ScrollControls pages={5} damping={0.12}>
                         <Overlay isControlling={isControlling} setIsControlling={setIsControlling} isZooming={isZooming} setIsZooming={setIsZooming} isShowStats={isShowStats} setIsShowStats={setIsShowStats} />
-                        <MyBG />
+                        <Bg />
                         <Sky sunPosition={[100, 20, 100]} distance={99999999} />
                         {/* <Environment preset='dawn' /> */}
                     </ScrollControls>
                 </Suspense>
             </Canvas>
+
 
         </>
     );
