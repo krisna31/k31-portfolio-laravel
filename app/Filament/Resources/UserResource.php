485cac40a270f7024407b47161f0b3a0e5b25fe6
Krisna31 <krisnaaaputraaa@gmail.com>
2023-12-26 16:22:14 +0700

    FIX - make password option in filament

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -34,8 +34,7 @@ class UserResource extends Resource
                     ->required(),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required(),
+                    ->password(),
             ]);
     }
 
