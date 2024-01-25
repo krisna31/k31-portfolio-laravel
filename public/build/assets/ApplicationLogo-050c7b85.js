311c662a907758ed6f726c4f526885306bbf818b
Krisna31 <krisnaaaputraaa@gmail.com>
2024-01-25 17:34:12 +0700

    build

---
+++ public/build/assets/ApplicationLogo-050c7b85.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-2daaf523.js";function i(o){return t.jsx("img",{...o,src:"/assets/pictures/logo.png",alt:"Application Logo"})}export{i as A};
--- public/build/assets/ApplicationLogo-504f870e.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-b9464bf4.js";function i(o){return t.jsx("img",{...o,src:"/assets/pictures/logo.png",alt:"Application Logo"})}export{i as A};
