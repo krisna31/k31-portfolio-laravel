9150960d3d49171c326797110891c1718ce96a4c
Krisna31 <krisnaaaputraaa@gmail.com>
2024-03-10 19:11:45 +0700

    FIX ~ error create logs at uuid PK

--- database/migrations/2024_03_10_060534_create_activity_log_table.php
+++ database/migrations/2024_03_10_060534_create_activity_log_table.php
@@ -12,11 +12,17 @@ class CreateActivityLogTable extends Migration
             $table->bigIncrements('id');
             $table->string('log_name')->nullable();
             $table->text('description');
-            $table->nullableMorphs('subject', 'subject');
-            $table->nullableMorphs('causer', 'causer');
+            // Replace nullableMorphs with string
+            $table->string('subject_id')->nullable();
+            $table->string('subject_type')->nullable();
+            $table->string('causer_id')->nullable();
+            $table->string('causer_type')->nullable();
             $table->json('properties')->nullable();
             $table->timestamps();
             $table->index('log_name');
+            // Add indexes for the morphs fields
+            $table->index(['subject_id', 'subject_type']);
+            $table->index(['causer_id', 'causer_type']);
         });
     }
 
