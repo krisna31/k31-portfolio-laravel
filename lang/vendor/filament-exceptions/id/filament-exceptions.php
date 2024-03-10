510165f91458f5217c718e42828af457c259dd03
Krisna31 <krisnaaaputraaa@gmail.com>
2024-03-10 18:27:38 +0700

    FIX ~ wrong translatation and build asset

---
+++ lang/vendor/filament-exceptions/id/filament-exceptions.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'labels' => [
+        'model' => 'Exception',
+        'model_plural' => 'Exceptions',
+        'navigation' => 'Error Logs',
+        'navigation_group' => 'Pengaturan',
+
+        'tabs' => [
+            'exception' => 'Exception',
+            'headers' => 'Headers',
+            'cookies' => 'Cookies',
+            'body' => 'Body',
+            'queries' => 'Queries',
+        ],
+    ],
+
+    'empty_list' => 'Horray! santai saja & nikmati 😎',
+
+    'columns' => [
+        'method' => 'Metode',
+        'path' => 'Path',
+        'type' => 'Tipe',
+        'code' => 'Kode',
+        'ip' => 'IP',
+        'occurred_at' => 'Terjadi pada',
+    ],
+
+];
---
+++ lang/vendor/filament-socialite/id/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'login-via' => 'Atau masuk melalui',
+
+    'login-failed' => 'Login gagal, silakan coba lagi.',
+
+    'user-not-allowed' => 'Email Anda bukan bagian dari domain yang diizinkan.',
+
+    'registration-not-enabled' => 'Pendaftaran pengguna baru tidak diizinkan.',
+];
