e8295025bf0119ea599751ab56028ff6d6446a18
krisna31 <krisnaaaputraaa@gmail.com>
2024-06-10 14:29:56 +0700

    add soft delete try in approval status

--- database/migrations/2024_01_04_115224_create_attende_statuses_table.php
+++ database/migrations/2024_01_04_115224_create_attende_statuses_table.php
@@ -16,7 +16,6 @@ return new class extends Migration {
             $table->string('description')->nullable()->default(null);
             $table->timestamps();
             $table->softDeletes();
-            $table->softDeletes();
         });
     }
 
