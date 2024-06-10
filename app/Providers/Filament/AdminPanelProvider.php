160f581188cd1e8e41e6c1952a28be2760133ac8
krisna31 <krisnaaaputraaa@gmail.com>
2024-06-10 15:04:11 +0700

    del nav bar backup spatie filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,7 +54,6 @@ class AdminPanelProvider extends PanelProvider
                     ->enableTwoFactorAuthentication(),
                 \BezhanSalleh\FilamentExceptions\FilamentExceptionsPlugin::make(),
                 new \RickDBCN\FilamentEmail\FilamentEmail(),
-                FilamentSpatieLaravelBackupPlugin::make(),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
