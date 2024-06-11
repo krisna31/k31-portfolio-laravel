a234490f823266ce5462020b694d511b42cc6483
krisna31 <krisnaaaputraaa@gmail.com>
2024-06-11 15:33:31 +0700

    try to add tabs

--- app/Filament/Resources/AttendeResource/Pages/ListAttendes.php
+++ app/Filament/Resources/AttendeResource/Pages/ListAttendes.php
@@ -6,19 +6,25 @@ use App\Filament\Resources\AttendeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
-class ListAttendes extends ListRecords
-{
+class ListAttendes extends ListRecords {
     protected static string $resource = AttendeResource::class;
 
-    protected function getHeaderActions(): array
-    {
+    protected function getHeaderActions(): array {
         return [
             Actions\CreateAction::make(),
         ];
     }
 
-    protected function getRedirectUrl(): string
-    {
+    protected function getRedirectUrl(): string {
         return $this->previousUrl ?? $this->getResource()::getUrl('index');
+    }
+
+    public function getTabs(): array {
+        return [
+            'Semua' => \Tabs\Tab::make('Semua')
+                ->modifyQueryUsing(function ($query) {
+                    return $query->whereDate('created_at', '=', now()->toDateString());
+                }),
+        ];
     }
 }
