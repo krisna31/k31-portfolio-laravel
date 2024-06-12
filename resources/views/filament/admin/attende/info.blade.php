8e38b0c54b7f0e698409f284bf78959394b9249a
krisna31 <krisnaaaputraaa@gmail.com>
2024-06-12 08:26:20 +0700

    hide when not absensi for verifikasi button

--- resources/views/filament/admin/attende/info.blade.php
+++ resources/views/filament/admin/attende/info.blade.php
@@ -1,6 +1,15 @@
-<div>
-    <h1>Data Status Persetujuan Presensi ini sudah berstatus <b>{{ str()->upper($record->approvalStatus->name) }}</b>
-    </h1>
-    <span style="font-size: .88rem; color: gray">Yang artinya sudah tidak bisa diubah lagi, silahkan hubungi administrator
-        anda jika ada kesalahan</span>
-</div>
+@if ($record->attende_time != null && $record->attende_status_id != null && $record->approval_status_id != null)
+    <div>
+        <h1>Pegawai ini <b>{{ str()->upper("belum melakukan presensi") }}</b>
+        </h1>
+        <span style="font-size: .88rem; color: gray">Yang artinya status persetujuan belum bisa diubah, silahkan hubungi administrator
+            anda jika ada kesalahan</span>
+    </div>
+@else
+    <div>
+        <h1>Data Status Persetujuan Presensi ini sudah berstatus <b>{{ str()->upper($record->approvalStatus->name) }}</b>
+        </h1>
+        <span style="font-size: .88rem; color: gray">Yang artinya sudah tidak bisa diubah lagi, silahkan hubungi administrator
+            anda jika ada kesalahan</span>
+    </div>
+@endif
