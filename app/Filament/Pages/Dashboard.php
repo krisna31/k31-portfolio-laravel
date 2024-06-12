e79902f79a9626157286277255be0881365d0710
krisna31 <krisnaaaputraaa@gmail.com>
2024-06-12 09:10:55 +0700

    try to add custom action in right dashbord to backup

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,6 +2,11 @@
 
 namespace App\Filament\Pages;
 
+use Alert;
+use Artisan;
+use Log;
+use Spatie\Backup\Helpers\Format;
+use Storage;
 
 class Dashboard extends \Filament\Pages\Dashboard {
     public function getHeading(): string {
@@ -14,8 +19,18 @@ class Dashboard extends \Filament\Pages\Dashboard {
                 ->button()
                 ->requiresConfirmation()
                 ->action(function() {
-                    dd('Backup Database');
-                    \Artisan::call('backup:run');
+                    try {
+                        // start the backup process
+                        Artisan::call('backup:run', ['--only-db' => 'true']);
+                        $output = Artisan::output();
+                        // log the results
+                        Log::info("Backpack\BackupManager -- new backup started from admin interface \r\n" . $output);
+                        return Alert::success('Backup Database Successful', 'The database has been successfully backed up.');
+                    } catch (Exception $e) {
+                        // log the error
+                        Log::error("Backpack\BackupManager -- new backup failed from admin interface \r\n" . $e->getMessage());
+                        return Alert::error('Backup Database Failed', 'An error occurred while trying to backup the database. Please check the logs for more information.');
+                    }
                 })
                 ->icon('heroicon-o-circle-stack')
                 ->color('info')
