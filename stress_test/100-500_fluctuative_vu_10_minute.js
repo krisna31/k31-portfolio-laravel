fb35b3408bb34dc3cb617e3981ad4a4b7606f32f
krisna31 <krisnaaaputraaa@gmail.com>
2024-12-06 16:12:18 +0700

    add stress test with k6

---
+++ stress_test/100-500_fluctuative_vu_10_minute.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  // Key configurations for Stress in this section
+  stages: [
+    { duration: '3m', target: 800 }, // traffic ramp-up from 1 to a higher 800 users over 3 minutes.
+    { duration: '2m', target: 150 },
+    { duration: '4m', target: 180 },
+    { duration: '30s', target: 180 },
+    { duration: '30s', target: 0 }, // ramp-down to 0 users
+  ],
+};
+
+export default () => {
+  const urlRes = http.get('https://test-api.k6.io');
+  sleep(1);
+};
