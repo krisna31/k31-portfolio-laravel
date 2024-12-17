63a4903f81c8eb6a26b7c5527a25fe10c3c71e1b
Krisna31 <krisnaaaputraaa@gmail.com>
2024-12-17 19:49:29 +0700

    FEAT ~ see the little increment not spike 500 i think, and add html format

--- stress_test/0-500_fluctuative_vu_10_minute.js
+++ stress_test/0-500_fluctuative_vu_10_minute.js
@@ -1,10 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { htmlReport } from "./html_report_bundle.js";
 
 export const options = {
     stages: [
-        { duration: '2m', target: 500 },
-        { duration: '1m', target: 300 },
+        { duration: '2m', target: 200 },
+        { duration: '1m', target: 10 },
         { duration: '2m', target: 150 },
         { duration: '4m', target: 320 },
         { duration: '30s', target: 180 },
@@ -16,3 +17,9 @@ export default () => {
     const urlRes = http.get('https://yoklah.absen.k31.my.id/');
     sleep(1);
 };
+
+export function handleSummary(data) {
+    return {
+        "summary_0_untill_200_vu_1_minute.html": htmlReport(data),
+    };
+}
---
+++ stress_test/20_vu_1_minute.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+import { sleep } from "k6";
+import { htmlReport } from "./html_report_bundle.js";
+
+export const options = {
+    vus: 20,
+    duration: "1m",
+};
+
+export default function () {
+    http.get('https://yoklah.absen.k31.my.id/');
+    sleep(1);
+}
+
+export function handleSummary(data) {
+    return {
+        "summary_20_vu_1_minute.html": htmlReport(data),
+    };
+}
