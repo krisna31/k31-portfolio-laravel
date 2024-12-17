ce9baef02cd3b837d0e1fe4536c28adf24c2c519
Krisna31 <krisnaaaputraaa@gmail.com>
2024-12-17 19:06:04 +0700

    FIX ~ use url https://yoklah.absen.k31.my.id/ and adjust to max 500 vu on fluctuative test

--- stress_test/100-500_fluctuative_vu_10_minute.js
+++ stress_test/100-500_fluctuative_vu_10_minute.js
@@ -4,7 +4,7 @@ import { sleep } from 'k6';
 export const options = {
   // Key configurations for Stress in this section
   stages: [
-    { duration: '3m', target: 800 }, // traffic ramp-up from 1 to a higher 800 users over 3 minutes.
+    { duration: '3m', target: 500 }, // traffic ramp-up from 1 to a higher 800 users over 3 minutes.
     { duration: '2m', target: 150 },
     { duration: '4m', target: 180 },
     { duration: '30s', target: 180 },
@@ -13,6 +13,6 @@ export const options = {
 };
 
 export default () => {
-  const urlRes = http.get('https://k31.my.id');
+  const urlRes = http.get('https://yoklah.absen.k31.my.id/');
   sleep(1);
 };
--- stress_test/500_vu_1_minute.js
+++ stress_test/500_vu_1_minute.js
@@ -8,6 +8,6 @@ export const options = {
 
 // The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
 export default function () {
-    const urlRes = http.get('https://k31.my.id');
+    const urlRes = http.get('https://yoklah.absen.k31.my.id/');
     sleep(1);
 }
